646f14268fd7ea56c43543b93cba1fc4108dcfce
Snehal_Renushe <72011262+snehalrenushe@users.noreply.github.com>
2024-04-09 11:46:28 +0530

    zod using http server

--- zod.js
+++ zod.js
@@ -13,15 +13,30 @@ const app = express();
 function validateInput(obj) {
   const schema = zod.object({
     email: zod.string().email(),
-    password: zod.string().min(8)
-  })
+    password: zod.string().min(8),
+  });
 
   const response = schema.safeParse(obj);
   console.log(response);
 }
 
-validateInput({
-  email: "snehalrenushe11@gmail.com",
-  password: "123456789"
-})
-// app.listen(3000);
+// validateInput({
+//   email: "snehalrenushe11@gmail.com",
+//   password: "123456789"
+// })
+
+app.post("/login", function (req, res) {
+  const response = validateInput(req.body);
+  console.log(response);
+
+  if (!response) {
+    res.json({
+      msg: "Inputs are wrong",
+    });
+  } else {
+    res.json({
+      msg: response,
+    });
+  }
+});
+app.listen(3000);
